45362a9e743688944e3d94adb993f0a3bc11d11d
lingfan <Lingfan.gao@insa-lyon.fr>
2015-10-28 00:16:11 +0100

    score predicates by Ling

--- Score.pl
+++ Score.pl
@@ -1,5 +1,19 @@
-score(B, R) :- countPts(B, R).
-%TODO
+%predicates to attribute score at each turn. They take in parameters:
+%B the state of the board at the chosen turn
+%P which player is playing
+%P1 the score of player1
+%P2 the score of player2
+%P2X the new score of both players
+
+score(B, P, P1, P2,P21,P22) :-    P = 1,                                        
+                          check(B,P1,P2),                               
+                          P21 is P1+1.                                   
+                                                                        
+score(B, P, P1, P2,P21,P22) :-    P = 2,                                       
+                          check(B,P1,P2),                              
+                          P22 is P2+1. 
+
+
 %an intermediary predicate that compares the number of full boxes
 %at the current turn, with the number of full boxes of the last turn
 %if there is a positive difference between the two, we attribute a score
@@ -8,3 +22,12 @@ score(B, R) :- countPts(B, R).
 countPts([], 0).
 countPts([15|Q], R) :- countPts(Q, R1), !, R is R1+1.
 countPts([_|Q], R) :- countPts(Q, R).
+
+%A predicate that returns true if the difference of full boxes between
+%the current turn and the sum of both scores is greater than zero
+%T1 refers to the turn
+%S1,S2 are the scores
+%Z is the difference
+check(T1,S1,S2) :- countPts(T1,X),Y is (S1+S2),Z is (X-Y),Z > 0.
+
+
