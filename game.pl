46f2bdfd95b2b1095e3f08cea89ea2ab76404c6f
lingfan <Lingfan.gao@insa-lyon.fr>
2015-11-01 03:21:30 +0100

    first version that seems to work

--- game.pl
+++ game.pl
@@ -1,3 +1,96 @@
+:- module(minimax, [minimax/3]).
+
+% minimax(Pos, BestNextPos, Val)
+% Pos is a position, Val is its minimax value.
+% Best move from Pos leads to position BestNextPos.
+minimax(Pos, BestNextPos, Val, TreeCounter) :-                     % Pos has successors
+    TreeCounter < 4,
+    addTreeCounter(TreeCounter, NextTreeCounter),
+    bagof(NextPos, move(Action,Index,Pos, NextPos), NextPosList), % We get all possible NextPos from move
+    best(NextPosList, BestNextPos, Val, NextTreeCounter), !.
+
+
+
+minimax(Pos, _, Val, _) :-                     % Pos has no successors
+    utility(Pos, Val).						   % End of the tree
+
+best([Pos], Pos, Val, TreeCounter) :-
+    minimax(Pos, _, Val, TreeCounter), !.
+
+best([Pos1 | PosList], BestPos, BestVal, TreeCounter) :-
+    minimax(Pos1, _, Val1, TreeCounter),
+    best(PosList, Pos2, Val2, TreeCounter),
+    betterOf(Pos1, Val1, Pos2, Val2, BestPos, BestVal).
+
+betterOf(Pos0, Val0, _, Val1, Pos0, Val0) :-   % Pos0 better than Pos1
+    min_to_move(Pos0),                         % MIN to move in Pos0
+    Val0 < Val1, !                             % MAX prefers the greater value
+    ;
+    max_to_move(Pos0),                         % MAX to move in Pos0
+    Val0 < Val1, !.                            % MIN prefers the lesser value
+
+betterOf(_, _, Pos1, Val1, Pos1, Val1).        % Otherwise Pos1 better than Pos0
+
+% min_to_move(+Pos)
+% True if the next player to play is the MIN player.
+min_to_move([user, _, _]).
+
+% max_to_move(+Pos)
+% True if the next player to play is the MAX player.
+max_to_move([computer, _, _]).
+
+
+%endPos(Player,Board)
+endPos([X1,X2,X3,X4,X5,X6,X7,X8,X9]):-
+	equal(X1,X2,X3,X4,X5,X6,X7,X8,X9),
+    X1 = 15,
+    X2 = 15,
+    X3 = 15,
+    X4 = 15,
+    X5 = 15,
+    X6 = 15,
+    X7 = 15,
+    X8 = 15,
+    X9 = 15.
+
+% utility(Pos, Val)
+% This will return the proper Val for each Pos
+utility([user,win,_],1). %toto win
+utility([computer,win,_],1). %titi win
+utility([_,draw,_],0). %no one win
+
+%simple heuristic that takes the potential score and divides it by the total score
+utility([user,B,Score], Val) :- Val is Score/9.
+utility([computer,B,Score], Val) :- Val is Score/9.
+
+% flatList(List[List], List)
+% This will override flatten, but it works only for
+% a list composed with a single element.
+flatList([L],L).
+flatList(L,L).
+
+% boardH(Board, BoardH)
+% This function will return the heuristic value 
+% associated with each position of the board.
+boardH([],[]).
+boardH([L1|L2],BH) :- flatList(L1,L1f), sumBits(L1f, S), boardH(L2,BH1), addList(S,BH1,BH).
+% sumBits(L,sum)
+% This function computes the sum of all bits in L
+sumBits([],0).
+sumBits([X|L], S) :- sumBits(L, S1), S is S1 + X.
+      
+%equal True if they are the same
+%Opti à faire : equal pour dimension illimitée
+equal(X,X,X,X,X,X,X,X,X).
+
+% addTreeCounter(TreeCounter,NewTreeCounter)
+% This function will be used to stop the search in the tree
+addTreeCounter(X,Y) :- Y is X + 1.
+
+% addList(X, List, List+X)
+% This function will add X to the List
+addList(X,L,[X|L]).
+
 game :-
         createList(Board,0,9),
 		play(Board,0,0,user).
@@ -35,8 +128,7 @@ play(Board,UserScore,ComputerScore,Player) :-
 userPlay(Board,NewBoard,OldScore,NewScore,NextPlayer) :- 
 	write('Play : '), 
 	read(Action), read(Index),
-    move(Action,Board,Index,[Value,ValueNeighbor],NewBoard),
-    score([Value,ValueNeighbor],OldScore,NewScore,user,NextPlayer).
+    move(Action,Index,[user,Board,OldScore],[NextPlayer,NewBoard,NewScore]).
 
 % Predicate computer player/5
 % +Board@ list: board game
@@ -46,10 +138,9 @@ userPlay(Board,NewBoard,OldScore,NewScore,NextPlayer) :-
 % -NextPlaying@ const: [ user | computer ] it's the next player turn
 computerPlay(Board,NewBoard,OldScore,NewScore,NextPlayer) :- 
     write('Computer playing...'), nl, 
-    % Here call for IA( Index1, Index2 ) algorithme :D
+    minimax([computer,Board,OldScore],[NextPlayer,NewBoard,NewScore],0,0),
     %putEdge(Board, [0, 0], [Value1, Value2], NewBoard),
-    NewBoard = Board,
-    score([1,1],OldScore,NewScore,computer,NextPlayer).
+    NewBoard = NextBoard.
 
 % Calculate Score
 score([Value1,Value2],OldScore,NewScore,CurrentPlayer,NextPlayer) :- 
@@ -74,74 +165,56 @@ nextPlayer(computer,user).
 % putEdge(Board, [Action, Index], [Value1, Value2], NewBoard) :- .
 
 
-% Move bottom edge
-move(bottom, Board, Index, NewValue, NewBoard) :-
-    % Extract if the bit of bottom is not setted
-    nth0(Index,Board,CurrentValue),
-    Bit is CurrentValue /\ 8, Bit \= 8, 
-    NewValue is CurrentValue + 8,
-    replace(Board, Index, NewValue, NewBoard).
+%(edge filled,value)
+%for filling in edges, corresponds to value that needs to be added 
+action(bottom,8).
+action(top,2).
+action(left,4).
+action(right,1).
 
-move(bottom, Board, Index, [NewValue, NewValueNeighbor], NewBoard) :-
-	member(Index, [6,7,8]),
-    move(bottom, Board, Index, NewValue, NewBoard),
-    NewValueNeighbor = 0;
+%(edge filled,index offset,value added to neighbour)
+%Tells us who is the neighbour and how its value should be modified
+neigh(bottom,3,2).
+neigh(top,-3,8).
+neigh(left,-1,1).
+neigh(right,1,4).
+
+%(edge filled,list of cases)
+%Action with the corresponding special border cases
+border(bottom,[6,7,8]).
+border(top,[0,1,2]).
+border(left,[0,3,6]).
+border(right,[2,5,8]).
+
+%for minimax:
+%[Player,Board,OldScore] represents Pos
+%[NextPlayer,NextBoard,NewScore] represents NextPos
+move(Action,Index,[Player,Board,OldScore],[NextPlayer,NextBoard,NewScore]) :-
+    moveaux(Action,Board,Index,[NewValue,ValueNeighbour],NextBoard),
+    score([NewValue,ValueNeighbour],OldScore,NewScore,Player,NextPlayer).
     
-    NIndex is Index + 3,
-    move(bottom, Board, Index, NewValue, R),
-    move(top, R, NIndex, NewValueNeighbor, NewBoard).
-
-% Move top edge
-move(top, Board, Index, [NewValue], NewBoard) :-
-    % Extract if the bit of top is not setted
+%predicate that actually handles the "physical move"
+moveaux(Action,Board,Index,[NewValue,ValueNeighbour],NewBoard) :-
     nth0(Index,Board,CurrentValue),
-    Bit is CurrentValue /\ 2, Bit \= 2, 
-    NewValue is CurrentValue + 2,
-    replace(Board, Index, NewValue, NewBoard).
+    CurrentValue < 15,
+    action(Action,Bit),
+    NewValue is CurrentValue + Bit,   
+    replace(Board,Index,NewValue,TmpBoard),
+	moveNeighbour(Action,TmpBoard,Index,ValueNeighbour,NewBoard).
 
-% Move top edge for Indexs [0,1,2]
-move(top, Board, Index, [NewValue, NewValueNeighbor], NewBoard) :-
-	member(Index, [0,1,2]),
-    move(top, Board, Index, [NewValue], NewBoard),
-    NewValueNeighbor = 0;
+%Predicate to handle to move of a neighbour box
+moveNeighbour(Action,Board,Index,NewValue,NewBoard):-
+    border(Action,Limit),
+    neigh(Action,Offset,Bit),
     
-    NIndex is Index - 3,
-    move(top, Board, Index, NewValue, R),
-    move(bottom, R, NIndex, [NewValueNeighbor, 0], NewBoard).
-
-% Move left edge
-move(left, Board, Index, NewValue, NewBoard) :-
-    nth0(Index,Board,CurrentValue),
-    Bit is CurrentValue /\ 4, Bit \= 4, 
-    NewValue is CurrentValue + 4,
-    replace(Board, Index, NewValue, NewBoard).
-
-move(left, Board, Index, [NewValue, NewValueNeighbor], NewBoard) :-
-    member(Index, [0,3,6]),
-    move(left, Board, Index, NewValue, NewBoard),
-    NewValueNeighbor = 0;
-    
-    NIndex is Index - 1,
-    move(left, Board, Index, NewValue, R),
-    move(right, R, NIndex, NewValueNeighbor, NewBoard).
-
-% Move right edge
-move(right, Board, Index, [NewValue], NewBoard) :-
-    % Extract if the bit of right is not setted
-    nth0(Index,Board,CurrentValue),
-    Bit is CurrentValue /\ 1, Bit \= 1, 
-    NewValue is CurrentValue + 1,
-    replace(Board, Index, NewValue, NewBoard).
-
-% Move right edge
-move(right, Board, Index, [NewValue, NewValueNeighbor], NewBoard) :-
-    member(Index, [2,5,8]),
-    move(right, Board, Index, NewValue, NewBoard),
-    NewValueNeighbor = 0;
-    
-    NIndex is Index + 1,
-    move(right, Board, Index, [NewValue], R),
-    move(left, R, NIndex, [NewValueNeighbor, 0], NewBoard).
+	%use of if -> then; else
+	%if our box is a border case
+    (  not(member(Index,Limit)) ->  (  NewIndex is Index+Offset,
+                                     nth0(NewIndex,Board,CurrentValue),
+                                     NewValue is CurrentValue+Bit,
+                                     replace(Board,NewIndex,NewValue,NewBoard));
+	%else do not change the board    
+	NewBoard = Board,NewValue = 0  ). 
     
    
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
