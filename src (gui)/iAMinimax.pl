840aab85a96e2814bfaee96d5a7fc0f559c7f936
Aiebobo <guethomas3@gmail.com>
2015-11-05 10:56:11 +0100

    IA minimax

--- src (gui)/iAMinimax.pl
+++ src (gui)/iAMinimax.pl
@@ -1,46 +1,50 @@
 :-module(iAMinimax,[minimax/5]).
 
-minimax(State, BestNextState , Val, 3, Dim) :-
-	heuristique(State, Val, Dim).
+minimax(State, BestNextState , Val, 2, Dim, PlayerBefore) :-
+	heuristique(State, Val, Dim, PlayerBefore).
 
-minimax(State, BestNextState, Val, TreeCounter, Dim) :-
+minimax(State, BestNextState, Val, TreeCounter, Dim,_) :-
 	getPossibilities(State,Dim,TreeCounter,NextStateList,NextTreeCounter),
 	best(State, NextStateList, BestNextState, Val, NextTreeCounter, Dim),!.
 
-minimax(State, _ , Val, _, Dim) :-
-	heuristique(State, Val, Dim).
+minimax(State, _ , Val, _, Dim, PlayerBefore) :-
+	heuristique(State, Val, Dim, PlayerBefore).
 	
-best(State, [StateOne], State, Val, _, Dim) :-
-	minimax(StateOne, _ , Val, _, Dim).
+best(State, [StateOne], StateOne, Val, _, Dim) :-
+    getPlayer(State,PlayerBefore),
+	minimax(StateOne, _ , Val, _, Dim,PlayerBefore).
 	
 best(State, [StateOne|NextStateList], BestState, BestVal, TreeCounter, Dim) :-
-	minimax(StateOne, _, ValOne, TreeCounter, Dim),
+	getPlayer(State,PlayerBefore),
+    minimax(StateOne, _, ValOne, TreeCounter, Dim,PlayerBefore),
 	best(State, NextStateList, StateTwo, ValTwo, TreeCounter, Dim),
 	betterOf(State, StateOne, ValOne, StateTwo, ValTwo, BestState, BestVal).
 	
 betterOf(State, StateOne, ValOne, _, ValTwo, StateOne, ValOne) :-
-	playerComputer(State),
 	ValOne>ValTwo.
 	
-betterOf(State, _, _, StateTwo, ValTwo, StateTwo, ValTwo).
+betterOf(_, _, _, StateTwo, ValTwo, StateTwo, ValTwo).
 	
-heuristique([_,_,[_,B,Score]], Val, Dim) :- not(Score = 0), Val is 1+(Score/(Dim*Dim)).
-heuristique([_,_,[_,B,0]], Val, Dim) :- random(Val).
+heuristique([_,_,[_,B,Score]], Val, Dim, computer) :- not(Score = 0), Val is 1+(Score/(Dim*Dim)).
+heuristique([_,_,[_,B,Score]], Val, Dim, user) :- not(Score = 0), Val is -1-(Score/(Dim*Dim)).
+heuristique([_,_,[_,B,0]], Val, Dim,_) :- random(Val).
 
-addTreeCounter(TreeCounter, Number, NextTreeCounter) :- NextTreeCounter is TreeCounter + Number.
+addTreeCounter(TreeCounter, NextTreeCounter) :- NextTreeCounter is TreeCounter + 1.
 
 getBoard([_,_,Board],Board).
+
+getPlayer(State,Player) :- getBoard(State,Board),dispatchBoard(Board,Player,_,_).
 
 dispatchBoard([Player,B,Score],Player,B,Score).
 
 playerComputer([_,_,[computer,_,_]]).
+playerPlayer([_,_,[user,_,_]]).
 
 getPossibilities(State,Dim,TreeCounter,NextStateList,NextTreeCounter):-
     getBoard(State, Board),
     dispatchBoard(Board,Player,B,Score),
-    addTreeCounter(TreeCounter, 1, NextTreeCounter),
     bagof([Action, Index, [Player, B2, Score2]], move(Action, Index, Dim, [Player, B, Score] , [Player, B2, Score2]), NextStateList)
 	;
     getBoard(State, Board),
-    addTreeCounter(TreeCounter, 3, NextTreeCounter),
+    addTreeCounter(TreeCounter, NextTreeCounter),
     findall([Action, Index, NextState], move(Action, Index, Dim, Board, NextState), NextStateList).
